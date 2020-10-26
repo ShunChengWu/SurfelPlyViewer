23c2bc8055233ca0f5bbc9a2f9a413844be44626
shun-cheng <shuncheng.wu91@gmail.com>
2020-10-26 19:03:01 +0100

    add support to render without raidus

--- libSurfelPlyViewer/SurfelPlyLoader.h
+++ libSurfelPlyViewer/SurfelPlyLoader.h
@@ -59,8 +59,9 @@ namespace Util {
 
         print_headers(file);
 
-        std::shared_ptr <tinyply::PlyData> point_data, label_data, normal_data, rgb_data, quality_data, radius_data;
+        std::shared_ptr <tinyply::PlyData> point_data, label_data, normal_data, rgb_data, /*quality_data,*/ radius_data;
 
+        bool bGoodRadius=true;
         try {
             std::vector <std::string> propertyKeys = {"x", "y", "z"};
             point_data = file.request_properties_from_element("vertex", propertyKeys);
@@ -80,14 +81,16 @@ namespace Util {
             rgb_data = file.request_properties_from_element("vertex", propertyKeys);
         } catch (const std::exception &e) { std::cerr << "tinyply exception: " << e.what() << std::endl; }
 
-        try {
-            quality_data = file.request_properties_from_element("vertex", {"quality"});
-        } catch (const std::exception &e) { std::cerr << "tinyply exception: " << e.what() << std::endl; }
+//        try {
+//            quality_data = file.request_properties_from_element("vertex", {"quality"});
+//        } catch (const std::exception &e) { std::cerr << "tinyply exception: " << e.what() << std::endl; }
 
         try {
             radius_data = file.request_properties_from_element("vertex", {"radius"});
-        } catch (const std::exception &e) { std::cerr << "tinyply exception: " << e.what() << std::endl; }
-
+        } catch (const std::exception &e) {
+            bGoodRadius = false;
+            std::cerr << "tinyply exception: " << e.what() << std::endl; }
+//        bGoodRadius = false;
 
         file.read(*file_stream);
 
@@ -95,6 +98,7 @@ namespace Util {
             float x,y,z;
         };
         std::vector<float3> points;
+        if (!point_data) return {};
         copyFromBuffer(point_data.get(), &points);
 
         std::vector<ushort> labels;
@@ -109,16 +113,17 @@ namespace Util {
         std::vector<uchar3> colors;
         copyFromBuffer(rgb_data.get(), &colors);
 
-        std::vector<float> qulity, radius;
-        copyFromBuffer(quality_data.get(), &qulity);
-        copyFromBuffer(radius_data.get(), &radius);
+        std::vector<float> /*qulity, */radius;
+//        copyFromBuffer(quality_data.get(), &qulity);
+        if(bGoodRadius)copyFromBuffer(radius_data.get(), &radius);
 
         std::vector<std::shared_ptr<Surfel>> surfels(points.size());
         Eigen::Vector3f centroid = {0.f,0.f,0.f};
         for(size_t i=0;i<points.size();++i ){
             surfels[i].reset(new Surfel());
             auto& surfel = surfels[i];
-            if (std::isnan(points[i].x) || std::isnan(points[i].y) || std::isnan(points[i].z)) {
+            if (std::isnan(points[i].x) || std::isnan(points[i].y) || std::isnan(points[i].z) ||
+            std::isinf(points[i].x) || std::isinf(points[i].y) || std::isinf(points[i].z)) {
                 printf("has nan\n");
                 surfel->pos.setZero();
                 surfel->is_valid = false;
@@ -143,15 +148,17 @@ namespace Util {
             if(labels.size() == points.size()){
                 surfel->label = labels[i];
             }
-            if(qulity.size() == points.size()){
-                surfel->label_confidence = qulity[i];
-            }
+//            if(qulity.size() == points.size()){
+                surfel->label_confidence = 100/*qulity[i]*/;
+//            }
             if(radius.size() == points.size()) {
                 surfel->radius = radius[i];
-            }
+            } else
+                surfel->radius = 10.f;
             surfel->is_valid = true;
             surfel->is_stable = true;
         }
+        std::cout << centroid.transpose() << "\n";
         centroid /= points.size();
         for(size_t i=0;i<points.size();++i )
             surfels[i]->pos -= centroid;
